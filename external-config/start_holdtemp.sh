55839f25aa6ae1b7e67c9c4a209fa03d85703c69
flkloste <flkloste@uos.de>
2017-08-14 09:58:18 +0200

    added external configuration files

---
+++ external-config/start_holdtemp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#sleep 20
+#sudo service pilight restart
+sudo iptables -A INPUT -s 127.0.01 -j ACCEPT
+#sudo /usr/local/sbin/pilight-daemon > /home/pi/pilightdeamon.log
+
+sleep 5
+#sudo python /home/pi/holdtemp/bt_controller.py
+sudo noip2
+sudo python /home/pi/python-server-brew-control/pythonserver.py > /home/pi/pythonserver.log 2>&1
+
