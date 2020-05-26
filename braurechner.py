08a023474df2dbfcc296e614a483bb2e416a83f0
flkloste <flkloste@uos.de>
2020-05-26 17:25:42 +0200

    Update braurechner.py

--- braurechner.py
+++ braurechner.py
@@ -23,4 +23,4 @@ if V_plus < 0:
   print("Error: Zielwert nicht erreichbar. Bitte Kochzeit verlängern")
 else:
   print("Wasser dazugeben: V_plus = %f" % V_plus)
-  print("Ausschlagwürze: V_nk = %f" % ((V_vk + V_plus)*0.9))
+  print("Ausschlagwürze: V_nk = %f" % ((V_vk + V_plus)*v_T))
